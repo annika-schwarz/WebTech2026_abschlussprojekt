4f09b58a6e3749509d131203e832af2de0806d38
Annika Schwarz <annika.schwarz@student.htw-berlin.de>
2026-06-28 21:19:29 +0200

    Grundstruktur

--- myConcerts/src/app/app.ts
+++ myConcerts/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Nav } from './nav/nav';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Nav, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
