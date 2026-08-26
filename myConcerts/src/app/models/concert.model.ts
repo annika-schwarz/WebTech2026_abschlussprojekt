0e9cde3b8f06e0a79e03ea0feb98853225d26138
Annika Schwarz <annika.schwarz@student.htw-berlin.de>
2026-08-26 19:33:14 +0200

    Fein-Tunig Beschreibung Konzert-Objekt

--- myConcerts/src/app/models/concert.model.ts
+++ myConcerts/src/app/models/concert.model.ts
@@ -1,10 +1,10 @@
 export interface Concert {
   id: string;           // String um UUIDs nutzen zu können
   artist: string;       //Haupt-Act
-  supportActs?: string; //Vorbands
+  supportActs?: string; //Optional: Vorbands
   venue: string;        //Veranstaltungsort 
   date: string;         //Datum des Konzerts
-  isPast: boolean;     //Ob das Konzert in der Vergangenheit liegt
-  rating?: number;      //Bewertung des Konzerts (1-5 Sterne)
-  comment?: string;     //Kommentar zum Konzert
+  isPast: boolean;      //Ob das Konzert in der Vergangenheit liegt
+  rating?: number;      //Optional: Bewertung des Konzerts (1-5 Sterne)
+  comment?: string;     //Optional: Kommentar zum Konzert
 }
