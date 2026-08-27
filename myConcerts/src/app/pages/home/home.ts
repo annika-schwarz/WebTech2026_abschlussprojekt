f989ce89ebd860e16b339b502163a6b7aaf1ef05
Annika Schwarz <annika.schwarz@student.htw-berlin.de>
2026-08-27 22:08:34 +0200

    Test Concert-Service in Home-Komponente

--- myConcerts/src/app/pages/home/home.ts
+++ myConcerts/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'; // Import der inject-Funktion von Angular, um die den ConcertService zu injizieren
+import { ConcertService } from '../../services/concert'; // Import des ConcertService, um auf die Konzerte zuzugreifen
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  // Injection des ConcertService, um auf die Konzerte zuzugreifen
+  concertService = inject(ConcertService);
+}
