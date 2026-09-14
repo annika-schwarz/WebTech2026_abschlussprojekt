dbfed7e7b3d02e39c5917d8eb77b3302d0678fa9
Annika Schwarz <annika.schwarz@student.htw-berlin.de>
2026-09-14 21:18:34 +0200

    add-concert Komponente fertig

--- myConcerts/src/app/pages/add-concert/add-concert.ts
+++ myConcerts/src/app/pages/add-concert/add-concert.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ConcertFormComponent } from '../../components/concert-form/concert-form';
+import { ConcertService } from '../../services/concert';
+import { Router } from '@angular/router';
+import { Concert } from '../../models/concert.model';
 
 @Component({
   selector: 'app-add-concert',
@@ -7,4 +10,15 @@ import { ConcertFormComponent } from '../../components/concert-form/concert-form
   templateUrl: './add-concert.html',
   styleUrl: './add-concert.css',
 })
-export class AddConcertComponent {}
+export class AddConcertComponent {
+
+  private concertService = inject(ConcertService); // injiziert den ConcertService, um auf die Methoden zum Speichern von Konzerten zuzugreifen
+  private router = inject(Router); // injiziert den Router, um nach dem Speichern des Konzerts zur Startseite zu navigieren
+
+  // Methode, die aufgerufen wird, wenn das Formular abgeschickt wird
+  onSave(formData: Omit<Concert, 'id' | 'isPast'>): void {
+    this.concertService.addConcert(formData); // ruft die Methode zum Hinzufügen eines Konzerts im ConcertService auf
+    this.router.navigate(['/']); // navigiert zur Startseite, nachdem das Konzert gespeichert wurde
+  }
+
+}
